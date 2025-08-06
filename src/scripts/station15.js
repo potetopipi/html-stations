128ac9cfae6e8e56acbb433127a0c40e22125bef
poteko <poteko@example.com>
2025-08-06 22:23:28 +0900

    追加

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,3 +1,5 @@
+const { textSpanEnd } = require("typescript");
+
 async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
@@ -6,11 +8,16 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+   const result = await new Promise((resolve) => {
+    setTimeout(() => {
+      userList.forEach(user => {
+        user.full_name = `${user.family_name} ${user.first_name}`;
+      });
+      resolve(userList);
+    }, 3000);
+  });
+
+  return result;
 }
 
-function test() {
-  return
-}
-
+getData().then(result => console.log(result));
